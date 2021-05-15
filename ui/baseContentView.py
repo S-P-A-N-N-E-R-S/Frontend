55417c0c3a731988eb65c37546cb431f241fc7a8
Dennis <dbenz@uni-osnabrueck.de>
2021-05-15 17:35:07 +0200

    Add message bar, refs #1

--- ui/baseContentView.py
+++ ui/baseContentView.py
@@ -1,3 +1,6 @@
+from qgis.gui import QgsMessageBar
+from qgis.core import Qgis
+
 
 class BaseContentView:
 
@@ -10,9 +13,25 @@ class BaseContentView:
         self.name = None
         self.dialog = dialog
 
+        # setup message bar
+        self.bar = QgsMessageBar()
+        self.dialog.content_widget.layout().insertWidget(0, self.bar)
+
     def setupWindow(self):
         """
         Sets up the contents and Slots of the content window
         :return:
         """
         raise NotImplementedError
+
+    def showError(self, msg):
+        self.bar.pushMessage("Error", msg, level=Qgis.Critical)
+
+    def showWarning(self, msg):
+        self.bar.pushMessage("Warning", msg, level=Qgis.Warning)
+
+    def showSuccess(self, msg):
+        self.bar.pushMessage("Success", msg, level=Qgis.Success)
+
+    def showInfo(self, msg):
+        self.bar.pushMessage("Info", msg, level=Qgis.Info)
