98fadf1f55db81e27c90896f86d899fce3813543
Dennis <dbenz@uni-osnabrueck.de>
2021-05-29 19:55:29 +0200

    Implement first base, refs #25

--- controllers/ogdfAnalysis.py
+++ controllers/ogdfAnalysis.py
@@ -1,5 +1,11 @@
-from .base import BaseController
+import os
 
+from qgis.core import QgsSettings
+
+from .base import BaseController
+# from ..network.client import Client
+from ..models.GraphBuilder import GraphBuilder
+from ..models.PGGraph import PGGraph
 
 class OGDFAnalysisController(BaseController):
 
@@ -10,6 +16,18 @@ class OGDFAnalysisController(BaseController):
         """
         super().__init__(view)
 
+        # set up client
+        self.settings = QgsSettings()
+        self.client = None
+        host = self.settings.value("protoplugin/host", None)
+        port = self.settings.value("protoplugin/port", None)
+
+        # try:
+        #     if host and port:
+        #         self.client = Client(host, port)
+        # except Exception as e:
+        #     print(e)
+
         self.view.addAnalysis("t-Spanner")
         self.view.addAnalysis("minimum spanner")
         self.view.addAnalysis("shortest path")
@@ -17,4 +35,48 @@ class OGDFAnalysisController(BaseController):
         self.view.addAnalysis("delaunay triangulation")
 
     def runJob(self):
-        pass
+        # if not self.client:
+        #     self.view.showWarning("Please configure the server!")
+        #     return
+
+        startNodeIndex = self.view.getStartNode()
+        endNodeIndex = self.view.getEndNode()
+
+        graph = self.__getGraph()
+        if not graph:
+            return
+
+        # try:
+        #     self.client.connect()
+        #
+        #     # shortPathRequest = ShortPathRequest(graph, startIndex, endIndex)
+        #     # msgLength = self.client.sendShortPathRequest(shortPathRequest)
+        #
+        #     graph = self.client.readShortPathResponse()
+        #
+        #     self.client.disconnectSocket()
+        # except Exception as e:
+        #     print(e)
+
+        # show graph in qgis
+        builder = GraphBuilder()
+        builder.setGraph(graph)
+        vertexLayer = builder.createVertexLayer(True)
+        edgeLayer = builder.createEdgeLayer(True)
+
+    def __getGraph(self):
+        if not self.view.hasInput():
+            self.view.showError("Please select a graph!")
+            return None
+
+        if self.view.isInputLayer():
+            # todo: graph layer handling
+            layer = self.view.getInputLayer()
+        else:
+            # if file path as input
+            path = self.view.getInputPath()
+            fileName, extension = os.path.splitext(path)
+            if extension == ".graphml":
+                graph = PGGraph()
+                graph.readGraphML(path)
+                return graph
--- views/ogdfAnalysisView.py
+++ views/ogdfAnalysisView.py
@@ -14,13 +14,45 @@ class OGDFAnalysisView(BaseContentView):
         # setup graph input
         self.dialog.ogdf_analysis_graph_input.setFilters(QgsMapLayerProxyModel.PluginLayer)
 
+        # set up graph file upload
+        self.dialog.ogdf_analysis_graph_input_tools.clicked.connect(
+            lambda: self._browseFile("ogdf_analysis_graph_input", "GraphML (*.graphml)")
+        )
+
         self.dialog.ogdf_analysis_run_btn.clicked.connect(self.controller.runJob)
 
     def getJobName(self):
         return self.dialog.ogdf_analysis_job_input.text()
 
-    def getGraph(self):
+    def hasInput(self):
+        return self.dialog.ogdf_analysis_graph_input.count() > 0
+
+    def isInputLayer(self):
+        """
+        True: if input is layer
+        False: if input is path
+        :return:
+        """
+        if self.dialog.ogdf_analysis_graph_input.currentLayer():
+            return True
+        return False
+
+    def getInputLayer(self):
+        """
+        Returns the current selected layer or none if path is selected
+        :return: Layer or None if path is selected
+        """
         return self.dialog.ogdf_analysis_graph_input.currentLayer()
+
+    def getInputPath(self):
+        """
+        Returns input path of selected file path in layer combobox
+        :return: Path to file or None if layer is selected
+        """
+        # assumed that only one additional item is inserted
+        if self.hasInput() and not self.isInputLayer():
+            return self.dialog.ogdf_analysis_graph_input.additionalItems()[0]
+        return None
 
     def getAnalysis(self):
         return self.dialog.ogdf_analysis_analysis_input.currentText(), self.dialog.create_graph_distance_input.currentData()
@@ -34,25 +66,13 @@ class OGDFAnalysisView(BaseContentView):
         return self.dialog.ogdf_analysis_crs_input.crs()
 
     def getStretch(self):
-        return self.dialog.ogdf_analysis_stretch_input.int()
+        return self.dialog.ogdf_analysis_stretch_input.value()
 
     def getStartNode(self):
-        return self.dialog.ogdf_analysis_start_input.currentText(), self.dialog.ogdf_analysis_start_input.currentData()
-
-    def addStartNode(self, startNode, userData=None):
-        self.dialog.ogdf_analysis_start_input.addItem(startNode, userData)
-
-    def addStartNodes(self, startNodes):
-        self.dialog.ogdf_analysis_start_input.addItems(startNodes)
+        return self.dialog.ogdf_analysis_start_input.value()
 
     def getEndNode(self):
-        return self.dialog.ogdf_analysis_end_input.currentText(), self.dialog.ogdf_analysis_end_input.currentData()
-
-    def addEndNode(self, endNode, userData=None):
-        self.dialog.ogdf_analysis_end_input.addItem(endNode, userData)
-
-    def addEndNodes(self, endNodes):
-        self.dialog.ogdf_analysis_end_input.addItems(endNodes)
+        return self.dialog.ogdf_analysis_end_input.value()
 
     # log
 
