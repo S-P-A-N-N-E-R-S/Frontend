786b10ed7b447b92ee763c6d3634f5cfb284bb81
Leon N <lnienhueser@uni-osnabrueck.de>
2021-06-29 07:52:44 +0000

    Feat/update to latest protocol version

--- responses/shortPathResponse.py
+++ responses/shortPathResponse.py
@@ -1,7 +1,7 @@
 from qgis.core import QgsPointXY
 
 from ..exceptions import ParseError
-from ..protocol.protos import GraphData_pb2
+from ..protocol.build import container_pb2, shortest_path_pb2
 
 
 class ShortPathResponse():
@@ -9,19 +9,22 @@ class ShortPathResponse():
     def __init__(self, graph=None):
         self.graph = graph
 
-        self.type = GraphData_pb2.ResponseContainer.ResponseType.SHORTEST_PATH
+        self.type = container_pb2.ResponseContainer.ResponseType.SHORTEST_PATH
 
     def parseProtoBuf(self, protoBuf):
         if protoBuf.type != self.type:
             raise ParseError("Invalid response type")
 
-        response = GraphData_pb2.ShortPathResponse()
+        response = shortest_path_pb2.ShortestPathResponse()
         protoBuf.response.Unpack(response)
 
         protoGraph = response.graph
 
         for vertex in protoGraph.vertexList:
-            self.graph.addVertex(QgsPointXY(vertex.x, vertex.y))
+            vertexX = response.vertexCoordinates[vertex.uid].x
+            vertexY = response.vertexCoordinates[vertex.uid].y
+            #TODO Parse possible z coordinates from protobuf
+            self.graph.addVertex(QgsPointXY(vertexX, vertexY))
 
         for edge in protoGraph.edgeList:
             self.graph.addEdge(edge.inVertexUid, edge.outVertexUid, [])
