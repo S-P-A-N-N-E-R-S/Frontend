4b51dabb48df66b7db7bb39d2a2608ba60d88869
jwittker <jwittker@uos.de>
2021-07-12 01:00:50 +0200

    Added distinction of left and right click, prepared keyPressEvent + canvasPressEvent for future deleteVertex function, refs #51

--- models/QgsGraphMapTool.py
+++ models/QgsGraphMapTool.py
@@ -2,7 +2,7 @@ from qgis.core import *
 from qgis.gui import QgsMapTool, QgsVertexMarker
 from qgis.utils import iface
 
-from qgis.PyQt.QtCore import QPoint
+from qgis.PyQt.QtCore import QPoint, Qt
 
 class QgsGraphMapTool(QgsMapTool):
     """QgsGraphMapTool should enable the user to edit a QgsGraphLayer
@@ -17,28 +17,65 @@ class QgsGraphMapTool(QgsMapTool):
 
         self.mLayer = layer
 
+        self.firstFound = False
+        self.ctrlPressed = False
+
     def canvasPressEvent(self, event):
         clickPosition = QPoint(event.pos().x(), event.pos().y())
 
         # used to convert canvas coordinates to map coordinates
         converter = QgsVertexMarker(iface.mapCanvas())
         clickPosition = converter.toMapCoordinates(clickPosition)
+        clickPosition = self.mLayer.mTransform.transform(clickPosition, QgsCoordinateTransform.ReverseTransform)
 
-        print("OldFeatureCount: ", self.mLayer.dataProvider().featureCount(), self.mLayer.mGraph.vertexCount())
+        if event.button() == Qt.LeftButton: # LeftClick
 
-        # add feature and vertex
-        feat = QgsFeature()
-        feat.setGeometry(QgsGeometry.fromPointXY(clickPosition))
+            # add feature and vertex
+            feat = QgsFeature()
+            feat.setGeometry(QgsGeometry.fromPointXY(clickPosition))
 
-        feat.setAttributes([self.mLayer.mGraph.vertexCount(), clickPosition.x(), clickPosition.y()])
-        self.mLayer.dataProvider().addFeature(feat)
+            feat.setAttributes([self.mLayer.mGraph.vertexCount(), clickPosition.x(), clickPosition.y()])
+            self.mLayer.dataProvider().addFeature(feat)
 
-        self.mLayer.mGraph.addVertex(clickPosition)
+            self.mLayer.mGraph.addVertex(clickPosition)
 
-        print("NewFeatureCount: ", self.mLayer.dataProvider().featureCount(), self.mLayer.mGraph.vertexCount())
+        elif event.button() == Qt.RightButton: # RightClick
 
+            # TODO: find a way to set a satisfying tolerance for the checkup
+            vertexId = self.mLayer.mGraph.findVertex(clickPosition, 100000)
+            if vertexId > 0 and not self.firstFound and not self.ctrlPressed:
+                # mark first found vertex
+                self.firstFound = True
+                self.firstFoundVertex = self.mLayer.mGraph.vertex(vertexId)
+                self.firstMarker = QgsVertexMarker(iface.mapCanvas())
+                self.firstMarker.setIconType(QgsVertexMarker.ICON_DOUBLE_TRIANGLE)
+                self.firstMarker.setCenter(clickPosition)
+            
+            elif self.firstFound:
+                # for now remove first found vertex, later get next marking spot as new position (maybe in left click instead?)
+                self.firstFound = False
+                iface.mapCanvas().scene().removeItem(self.firstMarker)
+                
+                del self.firstFoundVertex
+                del self.firstMarker
+
+            elif self.ctrlPressed: # CTRL + RightClick
+                # remove vertex if found on click
+                # TODO: add function in PGGraph deleteVertex
+                # self.mLayer.mGraph.deleteVertex(vertexId)
+
+        iface.mapCanvas().scene().removeItem(converter)
+        
         self.mLayer.triggerRepaint()
         self.mCanvas.refresh()
 
+    def keyPressEvent(self, event):
+        # TODO: seems like keyPressEvent needs canvasPressEvent beforehand?
+        if event.key() == Qt.Key_Control:
+            self.ctrlPressed = True
+
+    def keyReleaseEvent(self, event):
+        self.ctrlPressed = False
+    
     def isEditTool(self):
         return True
