0263e08ebb36fac162e460add8f0af3c7986f9e9
jwittker <jwittker@uos.de>
2021-07-25 13:55:16 +0200

    Adjusted graph controller to new return type of createVectorLayer

--- controllers/graph.py
+++ controllers/graph.py
@@ -128,13 +128,15 @@ class CreateGraphController(BaseController):
             else:
                 # if layer path as .shp
                 # create vector layer from graph layer
-                vectorLayer = graphLayer.createVectorLayer()
+                [vectorPointLayer, vectorLineLayer] = graphLayer.createVectorLayer()
 
                 # save vector layer to path
-                vectorLayer = helper.saveLayer(vectorLayer, vectorLayer.name(), "vector", savePath, extension)
+                # vectorPointLayer = helper.saveLayer(vectorPointLayer, vectorPointLayer.name(), "vector", savePath, extension)
+                vectorLineLayer = helper.saveLayer(vectorLineLayer, vectorLineLayer.name(), "vector", savepath, extension)
 
                 # add vector layer to project
-                QgsProject.instance().addMapLayer(vectorLayer)
+                QgsProject.instance().addMapLayer(vectorPointLayer)
+                QgsProject.instance().addMapLayer(vectorLineLayer)
 
         # add graph layer to project
         graphLayer.setName(graphName + "GraphLayer")
