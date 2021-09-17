ccc4a3e0e05170f40c3b9432671abe1353499bca
Tim <thartmann@uni-osnabrueck.de>
2021-09-17 04:43:21 +0000

    Update AStarBuildScript.sh

--- scripts/AStarBuildScript.sh
+++ scripts/AStarBuildScript.sh
@@ -2,16 +2,16 @@ echo "----- Script to build shared object -----"
 echo "----- Start build -----"
 
 echo "----- Try to delete existing .so file -----"
-sudo rm /usr/lib/python3/dist-packages/AStarC.cpython-39-x86_64-linux-gnu.so
+sudo rm /usr/lib/python3/dist-packages/AStarC.*.so
 
 echo "----- Execute setup.py -----"
 python3 scripts/setup.py build_ext --inplace
 
 echo "----- Copy .so file into dist-packages -----"
-sudo cp AStarC.cpython-39-x86_64-linux-gnu.so /usr/lib/python3/dist-packages
+sudo cp AStarC.*.so /usr/lib/python3/dist-packages
 
 echo "----- Remove .so file from model directory -----"
-rm AStarC.cpython-39-x86_64-linux-gnu.so
+rm AStarC.*.so
 echo "----- Remove created .c file from model directory -----"
 rm -r build
 
