4ecdd01a9713c32a99861f08af1fa727fb0b6bb5
Dennis <dbenz@uni-osnabrueck.de>
2021-10-10 14:46:26 +0200

    Add license texts to source files, closes #150

--- controllers/graph.py
+++ controllers/graph.py
@@ -1,13 +1,30 @@
+#  This file is part of the OGDF plugin.
+#
+#  Copyright (C) 2021  Dennis Benz, Tim Hartmann
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public
+#  License along with this program; if not, see
+#  https://www.gnu.org/licenses/gpl-2.0.html.
+
 import os
 
 from .base import BaseController
 
 from ..models.GraphBuilder import GraphBuilder
-from ..models.ExtGraph import ExtGraph
 from ..models.QgsGraphLayer import QgsGraphLayer
 from .. import helperFunctions as helper
 
-from qgis.core import QgsVectorLayer, QgsRasterLayer, QgsProject, QgsTask, QgsApplication, QgsMessageLog, Qgis
+from qgis.core import QgsProject, QgsTask, QgsApplication, QgsMessageLog, Qgis
 from qgis.utils import iface
 
 
