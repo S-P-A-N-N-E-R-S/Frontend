cc63ccf5a79cce181ab73fd9a26d7e8ff6fa5f93
Dennis <dbenz@uni-osnabrueck.de>
2021-12-07 21:10:55 +0100

    Run request creation on separate task, closes #154

--- views/ogdfAnalysisView.py
+++ views/ogdfAnalysisView.py
@@ -72,65 +72,6 @@ class OGDFAnalysisView(BaseView):
             # show parameters
             self.dialog.ogdf_analysis_parameters_box.show()
 
-    # def hasInput(self):
-    #     """
-    #     Returns true if input is not empty
-    #     :return:
-    #     """
-    #     if self.dialog.ogdf_analysis_graph_input.currentLayer() is not None:
-    #         return True
-    #
-    #     if len(self.dialog.ogdf_analysis_graph_input.additionalItems()) > 0:
-    #         return self.dialog.ogdf_analysis_graph_input.currentText() == self.dialog.ogdf_analysis_graph_input.additionalItems()[0]
-    #
-    #     return False
-
-    # def isInputLayer(self):
-    #     """
-    #     True: if input is layer
-    #     False: if input is path or empty
-    #     :return:
-    #     """
-    #     return self.dialog.ogdf_analysis_graph_input.currentLayer() is not None
-
-    # def getInputLayer(self):
-    #     """
-    #     Returns the current selected layer or none if path or empty is selected
-    #     :return: Layer or None if path or empty is selected
-    #     """
-    #     return self.dialog.ogdf_analysis_graph_input.currentLayer()
-
-    # def getInputPath(self):
-    #     """
-    #     Returns input path of selected file path in layer combobox
-    #     :return: Path to file or None if layer is selected
-    #     """
-    #     # assumed that only one additional item is inserted
-    #     if self.hasInput() and not self.isInputLayer():
-    #         return self.dialog.ogdf_analysis_graph_input.additionalItems()[0]
-    #     return None
-
-    # def getGraph(self):
-    #     """
-    #     Gets the graph containing in input. Return none if input has no graph
-    #     :return:
-    #     """
-    #     if self.hasInput():
-    #         if self.isInputLayer():
-    #             # return graph from graph layer
-    #             graphLayer = self.getInputLayer()
-    #             if isinstance(graphLayer, QgsGraphLayer) and graphLayer.isValid():
-    #                 return graphLayer.getGraph()
-    #         else:
-    #             # if file path as input
-    #             path = self.getInputPath()
-    #             fileName, extension = os.path.splitext(path)
-    #             if extension == ".graphml":
-    #                 graph = ExtGraph()
-    #                 graph.readGraphML(path)
-    #                 return graph
-    #     return None
-
     def getJobName(self):
         return self.dialog.ogdf_analysis_job_input.text()
 
